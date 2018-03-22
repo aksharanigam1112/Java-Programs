84ecb837dd508658f1bd8c5692fa4c7d626803bf
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2018-03-22 15:52:30 +0530

    Add files via upload

---
+++ continous.java
@@ -0,0 +1,15 @@
+class continous
+{
+ public static void main(int req)
+ {
+     int k,n,num=1;
+     for(k=req;k>=1;k--)
+     { 
+         for(n=1;n<=k;n++)
+         {
+             System.out.print("\t"+num++);
+            }
+            System.out.println();
+            }
+        }
+    }
