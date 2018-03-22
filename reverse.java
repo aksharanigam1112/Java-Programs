5b112b0d329ab73aa5a4d48214a3d3dcff060577
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2018-03-22 18:09:22 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,16 @@
+class reverse
+{
+ public static void main(int req)
+ {
+     int k,n;
+     for(k=req;k>=1;k--)
+     {
+         for(n=1;n<=k;n++)
+         {
+             System.out.print("\t"+n);
+            }
+            System.out.println();
+        }
+    }
+}
+            
