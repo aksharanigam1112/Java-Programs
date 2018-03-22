b88693aee5781603d52449227d9a3942f5af29fa
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2018-03-22 15:50:39 +0530

    Add files via upload

---
+++ chh.java
@@ -0,0 +1,18 @@
+class chh
+{
+public void cal()
+{
+         int  r;
+	char ch='A';
+	for (int p=0; p<=5;p++)
+	{
+               r=p++;
+	for (int a=0; a<=r; a++)
+            {
+               System.out.print(ch+" ");
+                  ch++;
+         }
+System.out.println("\n"+ch);
+}
+}
+}
