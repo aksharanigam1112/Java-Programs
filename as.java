bf89e8fc8df0d9606809dea99a97a22fbd89d607
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2018-03-22 15:43:24 +0530

    Add files via upload

---
+++ as.java
@@ -0,0 +1,10 @@
+class as
+{
+   public void cal()
+   {
+     System.out.println("-15/-5"+(-15/-5));
+     System.out.println("15/-5 "+(15/-5));
+     System.out.println("-15%-4 "+(-15%-4));
+     System.out.println("15%-4  "+(15%-4));
+    }
+}
