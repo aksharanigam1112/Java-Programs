2e195bd1fa4e4294c0f706667f17433f4d739ed0
Akshara Nigam <32769439+aksharanigam1112@users.noreply.github.com>
2018-03-22 16:23:24 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,17 @@
+class pattern
+{
+  public static void main(int req)
+  {
+      int k,n;
+      for(k=1 ; k<=req ; k++)
+      {
+          for(n=1;n<=k;n++)
+          {
+              System.out.print( "\t"+n);
+            }
+           System.out.println();
+        }
+    }
+}
+
+      
